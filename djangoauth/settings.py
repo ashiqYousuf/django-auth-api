28de91782bbbb1d2fa393d263783cfea785df36f
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2023-03-15 21:27:15 +0530

    code refactoring

--- djangoauth/settings.py
+++ djangoauth/settings.py
@@ -129,7 +129,7 @@ REST_FRAMEWORK = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ashiqhussainkumar143@gmail.com'
+EMAIL_HOST_USER = 'youremail@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_USE_TLS = True
 
